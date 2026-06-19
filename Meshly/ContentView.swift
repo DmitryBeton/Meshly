4261e3b57269e1e51e95cc86ec56147ba86abc21
Beton <dima41768@icloud.com>
2026-06-20 00:12:03 +0300

    Initial Commit

---
+++ Meshly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meshly
+//
+//  Created by Дмитрий Чалов on 20.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meshly/MeshlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshlyApp.swift
+//  Meshly
+//
+//  Created by Дмитрий Чалов on 20.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MeshlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
